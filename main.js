f6e4282795719c8dd1d283b165f2587f0090293b
Zarif-Syed <93282958+Zarif-Syed@users.noreply.github.com>
2023-01-16 14:47:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio: true});
-    classifier=ml5.soundClassifier('', modelReady);
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
 }
 
 function modelReady(){
     console.log("model loaded")
-    //classifier.classify(gotResults);
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        random_number_r=Math.floor(Math.random()*255)+1;
+        random_number_g=Math.floor(Math.random()*255)+1;
+        random_number_b=Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_label").innerHTML='I can hear - '+
+        results[0].label;
+        document.getElementById("result_confidence").innerHTML='Accuracy - '+
+        (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color="rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("
+        +random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img1=document.getElementById('img1');
+
+        if(results[0].label=="Barking"){
+            img1.src='dog_bark.png';
+        }
+        else{
+            img1.src='cat_meowing.png';
+        }
+    }
 }
